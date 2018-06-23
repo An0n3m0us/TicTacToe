90074ef383300c96f135d99d6b6a9833ebbc496f
An0n3m0us <an0n3m0us@hotmail.com>
2018-06-23 20:53:49 +0100

    Add variables; Add image for play button

--- main.lua
+++ main.lua
@@ -1,20 +1,31 @@
 width, height = love.graphics.getDimensions( )
 font = love.graphics.newFont("DejaVuSansMono-Bold.ttf", 50)
 
+local play = 0
+local turn = 1
+local clicks = 0
+local win = 0
+
 local theme = 0
 local sound = 0
 local colors = {}
 
-local fadeIn = 5
-local fadeIn2 = 5
+local fadeIn = 255
+local fadeIn2 = 255
 
 local crossAnim = {0, 0}
 local circleAnim = math.rad(-90)
 
 function love.load()
-   bgboard = love.graphics.newImage("bgboard.png")
-   bgwidth = bgboard:getWidth()
-   bgheight = bgboard:getHeight()
+    if theme == 0 then
+      bgboard = love.graphics.newImage("images/bgboard-white.png")
+      playB = love.graphics.newImage("images/playButton-white.png")
+    elseif theme == 1 then
+      bgboard = love.graphics.newImage("images/bgboard-black.png")
+      playB = love.graphics.newImage("images/playButton-black.png")
+    end
+    bgwidth, bgheight = bgboard:getWidth(), bgboard:getHeight()
+    playBwidth, playBheight = playB:getWidth(), playB:getHeight()
 end
 
 function menu()
@@ -22,7 +33,7 @@ function menu()
     love.graphics.printf("TIC TAC TOE", 0, 100, width, 'center')
 
     love.graphics.setColor(colors[3])
-    love.graphics.draw(bgboard, width/2-bgwidth/2, 200)
+    love.graphics.draw(bgboard, width/2-bgwidth/2, 250)
 end
 
 function cross(x, y)
@@ -62,27 +73,33 @@ function circle(x, y)
 end
 
 function love.draw()
-    love.graphics.setBackgroundColor(255, 255, 255)
-
-    love.graphics.setFont(font)
-
     if theme == 0 then
       colors[0] = {255, 255, 255}
-      colors[1] = {0, 0, 0, fadeIn}
+      colors[1] = {255, 255, 255, fadeIn}
       colors[2] = {0, 0, 0, fadeIn2}
       colors[3] = {255, 255, 255, fadeIn/10}
       colors[4] = {0, 0, 0}
       colors[5] = {255, 255, 255}
+    elseif theme == 1 then
+      colors[0] = {0, 0, 0}
+      colors[1] = {255, 255, 255, fadeIn}
+      colors[2] = {255, 255, 255, fadeIn2}
+      colors[3] = {255, 255, 255, fadeIn/5}
+      colors[4] = {255, 255, 255}
+      colors[5] = {0, 0, 0}
     end
+
+    love.graphics.setBackgroundColor(colors[0])
+
+    love.graphics.setFont(font)
 
     menu()
 
     love.graphics.setLineStyle("smooth")
     love.graphics.setLineWidth(5)
 
-    love.graphics.setColor(0, 0, 0)
-    love.graphics.rectangle("line", width/2-75, 330, 150, 50, math.rad(90), math.rad(90), 500)
-    love.graphics.printf("PLAY", 0, 327.5, width, 'center')
+    love.graphics.setColor(colors[1])
+    love.graphics.draw(playB, width/2-playBwidth/4, 375.5, 0, 0.5)
 end
 --[[ ANIMATION FOR CIRCLES AND CROSSES
 if crossAnim[1] < 35 then
