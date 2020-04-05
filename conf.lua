efafeb6dd4368b9dd31542e99c308f9d865d5921
An0n3m0us <an0n3m0us@hotmail.com>
2020-04-05 20:27:47 +0100

    Add license

--- conf.lua
+++ conf.lua
@@ -1,5 +1,7 @@
 function love.conf(settings)
+    settingsversion = "0.10.2"
     settings.window.title = "Tic Tac Toe"
+    settings.window.icon = "icon.png"
     settings.window.width = 1280
     settings.window.height = 720
 end
